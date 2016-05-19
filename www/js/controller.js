0a20d382c676e00da5a5c97dc14d171928de723d
nazmiyu <thanyffak@hotmail.co.jp>
2016-05-19 17:15:55 +0900

    third

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,9 +1,26 @@
-var app = angular.module('app', ['ngResource']);
+var app = angular.module('app', ['ngResource','ngRoute']).config(['$locationProvider', function($locationProvider) {
 
-app.controller('MainCtrl', function($scope, $resource, $window, $location) {
-  var location = $location;
-  console.log(location);
-  var Project = $resource('./api.php:id', {id: '@id'});
-  $scope.projects = Project.query();
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
+}]);
+
+app.controller('MainCtrl', function($scope, $resource, $window, $location, $route) {
+  var idParam = $location.search().id;
+  if (idParam) {
+    var Project = $resource('./api.php?id=' +  idParam, {id: '@id'});
+    $scope.projects = Project.query();
+  } else {
+    var Project = $resource('./api.php:id', {id: '@id'});
+    $scope.projects = Project.query();
+  }
+
+  $scope.click = function(id) {
+    $scope.id = id;
+    $location.path('/').search({'id': id});
+    $route.reload();
+  }
 
 });
